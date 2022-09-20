c659bb8973a87e4db9719d11de269c85bd2caf0c
Harsha <harsha0770@gmail.com>
2022-09-20 21:45:12 +0530

    Inital Commit

---
+++ src/main/java/com/valuemedi/projectenna/ProjectEnnaApplication.java
@@ -0,0 +1,13 @@
+package com.valuemedi.projectenna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEnnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEnnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valuemedi/projectenna/ProjectEnnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valuemedi.projectenna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEnnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
